94126d49df483cd0aa406c76c1ab95305029fc25
Mengtong Shi <mengtongshi@gmail.com>
2025-03-28 21:41:03 -0400

    Update model.py

--- src/neural_decoder/model.py
+++ src/neural_decoder/model.py
@@ -18,7 +18,7 @@ class GRUDecoder(nn.Module):
         kernelLen=14,
         gaussianSmoothWidth=0,
         bidirectional=False,
-        use_layer_norm=True,
+        use_layer_norm=False,
     ):
         super(GRUDecoder, self).__init__()
 
